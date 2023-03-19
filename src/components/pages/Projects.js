7102af4d313bda9a5b0febb115a68aa4c13d4dca
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-03-19 13:10:16 -0400

    added new card on projects. added <a> to include link to github pages on projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,26 +3,55 @@ import barcode from "../../images/barcode.jpg";
 
 export default function Projects() {
 	return (
-		<div className="card shadow">
-			<div className="row">
-				<img
-					id="barcode"
-					className="col-md-4"
-					src={barcode}
-					alt="kitchen inventory"
-				></img>
-				<div className="col-md-8">
-					<div className="card-body">
-						<h5 className="card-title">Kitchen Inventory</h5>
-						<p className="card-text">
-							Inventory management system developed to aid a local elementary
-							school transition from paper inventory to electronic, which
-							utilizes the user’s phone as a barcode scanner and stores data
-							using MongoDB
-						</p>
+		<div>
+			<a
+				href="https://github.com/jeremystevens515/lunchroom-inventory-app"
+				className="card shadow"
+			>
+				<div className="row">
+					<img
+						id="barcode"
+						className="col-md-4"
+						src={barcode}
+						alt="kitchen inventory"
+					></img>
+					<div className="col-md-8">
+						<div className="card-body">
+							<h5 className="card-title">Kitchen Inventory</h5>
+							<p className="card-text">
+								Inventory management system developed to aid a local elementary
+								school transition from paper inventory to electronic, which
+								utilizes the user’s phone as a barcode scanner and stores data
+								using MongoDB. NOTE: this link will take you to the github
+								repository, and not the deployed application, as the application
+								is currently being used by the client and is password protected.
+							</p>
+						</div>
 					</div>
 				</div>
-			</div>
+			</a>
+			<a
+				href="https://github.com/jeremystevens515/eCommerce-backend"
+				className="card shadow"
+			>
+				<div className="row">
+					<img
+						id="barcode"
+						className="col-md-4"
+						src={barcode}
+						alt="kitchen inventory"
+					></img>
+					<div className="col-md-8">
+						<div className="card-body">
+							<h5 className="card-title">eCommerce Backend</h5>
+							<p className="card-text">
+								Back end application using Express.js, MySQL, and Sequelize.
+								RESTful API routes written using Sequelize.
+							</p>
+						</div>
+					</div>
+				</div>
+			</a>
 		</div>
 	);
 }
