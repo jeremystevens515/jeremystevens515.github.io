1b23a8d56f93428a9c4ce17223d6274f3a5f625b
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-07-27 14:34:58 -0500

    made decision to forego bottom slide section as complexity is not worth it. creating separate page for software

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,94 +14,63 @@ export default function About() {
 	}, []);
 
 	if (loaded) {
-		let observer = new IntersectionObserver((entries) => { //callback function to execute when the element intersects with the viewport
+		// animate welcome and color mask for name upon page loading
+		const timeline = anime.timeline({
+			easing: "easeInOutQuad",
+		});
+
+		timeline
+			.add({
+				targets: ".welcome-section",
+				translateY: [-500, 0],
+				filter: ["blur(10px)", "blur(0px)"],
+			})
+			.add({
+				targets: "#name",
+				backgroundPosition: "100% 0%",
+				delay: 250,
+				duration: 1000,
+
+			});
+
+		//animate mid section elements when they intersect with the viewport
+		let Observer = new IntersectionObserver((entries) => { //callback function to execute when the element intersects with the viewport
 			entries.forEach((entry) => {
-				if (entry.isIntersecting) {
-					console.log(entry.target);
+				if (entry.isIntersecting) { //if the element is intersecting with the viewport
 					entry.target.classList.add("show");
+					// entry.target.classList.remove("hide");
 				}
 			});
-		}, { threshold: 0.25 });
+		}, { threshold: 0.5 });
+
+		let elements = document.querySelectorAll(".hide");
+		elements.forEach((element) => {
+			Observer.observe(element);
+		});
 
 
-		let hiddenEls = document.querySelectorAll(".hide"); //select all elements with the class "hide"
-		console.log(hiddenEls);
-		hiddenEls.forEach((el) => { observer.observe(el); }); //for each element with the class "hide", observe it
-
-		anime({
-			targets: "#name",
-			backgroundPosition: "100% 0%",
-			duration: 2000,
-			easing: "easeInOutQuad",
-			delay: 500,
-		})
 	}
-
 	return (
 		<div id="container">
-			< section className="hide" >
-				<p className="hide text-4xl text-white tracking-wider p-4">Welcome.</p>
-				<p className="hide text-6xl text-white tracking-wide">I'm <span id="name">Jeremy</span></p>
+
+			< section className="welcome-section" >
+				<p className="text-4xl text-white tracking-wide p-4">Welcome.</p>
+				<p className="text-6xl text-white tracking-wide">I'm <span id="name">Jeremy</span></p>
 			</section >
-			<section className="hide">
-				<p className="hide text-4xl text-white">I'm a software developer</p>
-				<p className="hide text-4xl text-white">certified in full-stack web development</p>
-				<p className="hide text-4xl text-white">and C++</p>
+
+			<section className="hide mid-section">
+				<p className="text-4xl text-white">I'm a software developer</p>
+				<p className="text-4xl text-white">certified in full-stack web development</p>
+				<p className="text-4xl text-white">and C++</p>
 			</section>
+
 			<section className="hide button-section">
-
-				<div>
-					<p>about me</p>
-					<button onClick={() => {
-						anime({
-							targets: ".button-section",
-							translateX: "-22.5%",
-							duration: 350,
-							easing: "easeInSine",
-						})
-					}} className="btn btn-ghost">Back &gt;</button>
+				<p className="text-4xl text-white text-center">I love to learn</p>
+				<p className="text-4xl text-white text-center">and understand how everything works</p>
+				<div className="p-4 text-center">
+					<Link className="btn btn-ghost">&lt; More about me</Link>
+					<Link to="/software" className="btn btn-ghost">Projects &gt;</Link>
 				</div>
-
-				<div>
-					<p className="hide text-4xl text-white">I love to learn</p>
-					<p className="hide text-4xl text-white">and understand how everything works</p>
-					<div className="hide p-4">
-						<button onClick={() => {
-							anime({
-								targets: ".button-section",
-								translateX: "24%",
-								duration: 350,
-								easing: "easeInSine",
-							})
-						}} className="btn btn-ghost">&lt; More about me</button>
-						<button onClick={() => {
-							anime({
-								targets: ".button-section",
-								translateX: "-72.5%",
-								duration: 350,
-								easing: "easeInSine",
-							});
-						}} className="btn btn-ghost">Projects &gt;</button>
-					</div>
-
-				</div>
-				<div className="project-section">
-					<h2>Projects</h2>
-					<div className="projects">
-						<div className="project">MERN</div>
-						<div className="project">MERN</div>
-						<div className="project">.NET</div>
-					</div>
-					<button onClick={() => {
-						anime({
-							targets: ".button-section",
-							translateX: "-22.5%",
-							duration: 350,
-							easing: "easeInSine",
-						})
-					}} className="btn btn-ghost">Back &lt;</button>
-				</div>
-
 			</section>
 		</div >
 
--- src/pages/Software.js
+++ src/pages/Software.js
@@ -5,69 +5,81 @@ import socialMedia from "../images/social-media.jpg";
 
 export default function Software() {
 	return (
-		<div className="grid grid-cols-3 gap-4 justify-center items-center">
-			<div className="card bg-primary shadow-xl">
-				<figure>
-					<img className="project-img bg-center bg-cover" src={lunchroom} alt="barcode" />
-				</figure>
-				<div className="card-body text-center">
-					<h2 className="text-secondary text-2xl font-medium">MERN Stack</h2>
-					<p className="text-info">
-						Inventory management system utilizing the entire MERN stack of technologies- MongoDB, Express, React, & Node.
-					</p>
-					<div>
-						<a
-							href="https://github.com/jeremystevens515/lunchroom-inventory-app"
-							className="btn btn-primary"
-						>
-							GitHub Repository
-						</a>
+		<div className="">
+			<div className="hidden">
+				<div className="card bg-primary shadow-xl">
+					<figure>
+						<img className="project-img bg-center bg-cover" src={lunchroom} alt="barcode" />
+					</figure>
+					<div className="card-body text-center">
+						<h2 className="text-secondary text-2xl font-medium">MERN Stack</h2>
+						<p className="text-info">
+							Inventory management system utilizing the entire MERN stack of technologies- MongoDB, Express, React, & Node.
+						</p>
+						<div>
+							<a
+								href="https://github.com/jeremystevens515/lunchroom-inventory-app"
+								className="btn btn-primary"
+							>
+								GitHub Repository
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div className="card bg-primary shadow-xl">
+					<figure>
+						<img className="project-img" src={ecommerce} alt="barcode" />
+					</figure>
+					<div className="card-body text-center">
+						<h2 className="text-secondary text-2xl font-medium">eCommerce Website</h2>
+						<p className="text-info">
+							eCommerce site built with MongoDB, Express, React, & Node.
+						</p>
+						<p>
+							Coming soon.
+						</p>
+						<div>
+							<a
+								href="https://github.com/jeremystevens515/"
+								className="btn btn-primary"
+							>
+								GitHub Repository
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div className="card bg-primary shadow-xl">
+					<figure>
+						<img className="project-img" src={socialMedia} alt="barcode" />
+					</figure>
+					<div className="card-body text-center">
+						<h2 className="text-secondary text-2xl font-medium">Social Media Clone</h2>
+						<p className="text-info">
+							Social media clone built with .NET.
+						</p>
+						<p> Coming soon.</p>
+						<div>
+							<a
+								href="https://github.com/jeremystevens515/"
+								className="btn btn-primary"
+							>
+								GitHub Repository
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
 
-			<div className="card bg-primary shadow-xl">
-				<figure>
-					<img className="project-img" src={ecommerce} alt="barcode" />
-				</figure>
-				<div className="card-body text-center">
-					<h2 className="text-secondary text-2xl font-medium">eCommerce Website</h2>
-					<p className="text-info">
-						eCommerce site built with MongoDB, Express, React, & Node.
-					</p>
-					<p>
-						Coming soon.
-					</p>
-					<div>
-						<a
-							href="https://github.com/jeremystevens515/"
-							className="btn btn-primary"
-						>
-							GitHub Repository
-						</a>
-					</div>
+			<div className="software">
+				<h2>Projects</h2>
+				<div className="projects">
+					<a href="https://github.com/jeremystevens515/lunchroom-inventory-app" className="project">MERN</a>
+					<a href="https://github.com/jeremystevens515/" className="project">MERN</a>
+					<a href="https://github.com/jeremystevens515/" className="project">.NET</a>
 				</div>
-			</div>
-
-			<div className="card bg-primary shadow-xl">
-				<figure>
-					<img className="project-img" src={socialMedia} alt="barcode" />
-				</figure>
-				<div className="card-body text-center">
-					<h2 className="text-secondary text-2xl font-medium">Social Media Clone</h2>
-					<p className="text-info">
-						Social media clone built with .NET.
-					</p>
-					<p> Coming soon.</p>
-					<div>
-						<a
-							href="https://github.com/jeremystevens515/"
-							className="btn btn-primary"
-						>
-							GitHub Repository
-						</a>
-					</div>
-				</div>
+				<button className="btn btn-ghost">Back &lt;</button>
 			</div>
 		</div>
 	);
