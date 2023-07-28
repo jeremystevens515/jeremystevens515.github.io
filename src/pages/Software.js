5880a482143c40adc91cb137d8945d5b303591ce
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-07-27 22:29:33 -0500

    made projects page

--- src/pages/Software.js
+++ src/pages/Software.js
@@ -1,86 +1,52 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
 import lunchroom from "../images/lunchroom-inventory.jpg";
 import ecommerce from "../images/ecommerce.jpg";
 import socialMedia from "../images/social-media.jpg";
 
+import anime from "animejs";
+import { Link } from "react-router-dom";
+
 export default function Software() {
+	const [loaded, setLoaded] = useState(false);
+	useEffect(() => {
+		setLoaded(true);
+	}, []);
+
+	if (loaded) {
+		anime({
+			targets: ".project",
+			duration: 1250,
+			translateX: [-1000, 0],
+			filter: ["blur(10px)", "blur(0px)"],
+			opacity: [0, 1],
+			delay: anime.stagger(100, { easing: "easeOutSine" }),
+			loop: false,
+		})
+	}
+
 	return (
-		<div className="">
-			<div className="hidden">
-				<div className="card bg-primary shadow-xl">
-					<figure>
-						<img className="project-img bg-center bg-cover" src={lunchroom} alt="barcode" />
-					</figure>
-					<div className="card-body text-center">
-						<h2 className="text-secondary text-2xl font-medium">MERN Stack</h2>
-						<p className="text-info">
-							Inventory management system utilizing the entire MERN stack of technologies- MongoDB, Express, React, & Node.
-						</p>
-						<div>
-							<a
-								href="https://github.com/jeremystevens515/lunchroom-inventory-app"
-								className="btn btn-primary"
-							>
-								GitHub Repository
-							</a>
-						</div>
-					</div>
-				</div>
-
-				<div className="card bg-primary shadow-xl">
-					<figure>
-						<img className="project-img" src={ecommerce} alt="barcode" />
-					</figure>
-					<div className="card-body text-center">
-						<h2 className="text-secondary text-2xl font-medium">eCommerce Website</h2>
-						<p className="text-info">
-							eCommerce site built with MongoDB, Express, React, & Node.
-						</p>
-						<p>
-							Coming soon.
-						</p>
-						<div>
-							<a
-								href="https://github.com/jeremystevens515/"
-								className="btn btn-primary"
-							>
-								GitHub Repository
-							</a>
-						</div>
-					</div>
-				</div>
-
-				<div className="card bg-primary shadow-xl">
-					<figure>
-						<img className="project-img" src={socialMedia} alt="barcode" />
-					</figure>
-					<div className="card-body text-center">
-						<h2 className="text-secondary text-2xl font-medium">Social Media Clone</h2>
-						<p className="text-info">
-							Social media clone built with .NET.
-						</p>
-						<p> Coming soon.</p>
-						<div>
-							<a
-								href="https://github.com/jeremystevens515/"
-								className="btn btn-primary"
-							>
-								GitHub Repository
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div className="software">
-				<h2>Projects</h2>
+		<div className="software">
+			<h2 className="text-6xl">Projects</h2>
+			<div className="">
 				<div className="projects">
-					<a href="https://github.com/jeremystevens515/lunchroom-inventory-app" className="project">MERN</a>
-					<a href="https://github.com/jeremystevens515/" className="project">MERN</a>
-					<a href="https://github.com/jeremystevens515/" className="project">.NET</a>
+					<div className="project">
+						<a href="https://jeremystevens515.github.io/ssi">React.js</a>
+					</div>
+					<div className="project">
+						<a href="https://github.com/jeremystevens515/lunchroom-inventory-app">MERN</a>
+					</div>
+					<div className="project">
+						<a href="https://github.com/jeremystevens515/" >MERN</a>
+					</div>
+					<div className="project">
+						<a href="https://github.com/jeremystevens515/" >.NET</a>
+					</div>
 				</div>
-				<button className="btn btn-ghost">Back &lt;</button>
 			</div>
+
+			<Link to="/" className="btn btn-ghost">Back &lt;</Link>
 		</div>
 	);
 }
